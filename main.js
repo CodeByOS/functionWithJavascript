0eac5276e5fe9e9b00a0f05ee0cb942b1db59507
Oussama <saidi.oussama2004@gmail.com>
2025-02-08 12:09:25 +0100

    Update

--- main.js
+++ main.js
@@ -16,8 +16,12 @@ console.log(countChars("Oussama Saidi"));
 console.log('#'.repeat(30));
 
     //? Capitalize Words
-function capitalizeWords(str) {
-    return str.toUpperCase();
+function capitalizeWords(sentence) {
+    let words = sentence.split(" ");
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+    }
+    return words.join(" ");
 }
 console.log(capitalizeWords("gOmYcoDe"));
 
